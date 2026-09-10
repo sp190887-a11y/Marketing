6bcbcd79c9d9520e0ed1f58cd28a41978bad57eb
sp190887-a11y <s.p.190887@gmail.com>
2026-09-10 19:30:54 +0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='dela-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./payload.txt','./config.js','./manifest.webmanifest']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request).catch(()=>caches.match('./')));});
