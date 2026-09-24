1a97b1435ca8dbfa10e7a1d343f61504f038c195
sp190887-a11y <s.p.190887@gmail.com>
2026-09-24 07:26:41 +0300

    AMP Club: register manager assignment API

--- amp-club-backend/api/app.py
+++ amp-club-backend/api/app.py
@@ -1,8 +1,10 @@
 from .index import app
 from .crm import crm_bp
+from .managers import manager_bp
 from .mfa import mfa_bp
 
 app.register_blueprint(crm_bp)
+app.register_blueprint(manager_bp)
 app.register_blueprint(mfa_bp)
 
 __all__ = ["app"]
